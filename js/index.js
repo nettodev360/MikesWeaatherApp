759ba13738c17a909fd866f112457d71569dac8e
Michael Netto <netto.dev360@gmail.com>
2022-11-19 09:49:04 -0500

    updated 'feels_like'.. non functional and removed Loading indicator.

--- js/index.js
+++ js/index.js
@@ -12,13 +12,14 @@ let weather = {
     displayWeather: function(data) {
         const { name } = data;
         const { icon, description } = data.weather[0];
-        const { temp, humidity } = data.main;
+        const { temp, humidity, feels_like } = data.main;
         const { speed } = data.wind;
         console.log(name,icon,description,temp,humidity,speed);
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon + ".png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°C";
+        // document.querySelector(".feels_like").innerText =  temp + "°C";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
     },
@@ -35,4 +36,4 @@ document.querySelector(".search-bar").addEventListener("keyup", function (event)
     if (event.key == "Enter") {
         weather.search();
     }
-})
+}) 
